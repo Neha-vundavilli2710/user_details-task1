e6d819ba66d7741620eceedfe95ca49ac69f9dba
Neha-vundavilli2710 <23p31a4272@acet.ac.in>
2026-01-07 19:40:34 +0530

    fix: correct case of User model import

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const User = require("./models/User");
+const User = require("./models/user");
 
 const app = express();
 app.use(cors());
